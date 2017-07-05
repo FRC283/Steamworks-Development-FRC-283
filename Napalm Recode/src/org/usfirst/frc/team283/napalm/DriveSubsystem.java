8ab9faed968140a460574f63204d29465335c787
Ben Ranson <benr444@bellsouth.net>
2017-07-05 16:04:29 -0400

    Initial Commit
    Ben
    Rebuilt repository due to lack of .project and .classpath files on older
    commits of former repository. This new repo won't have those issues.

---
+++ Napalm Recode/src/org/usfirst/frc/team283/napalm/DriveSubsystem.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team283.napalm;
+
+import edu.wpi.first.wpilibj.Talon;
+
+public class DriveSubsystem
+{
+	//Constants
+	private static final double DEADZONE = 0.1;
+	private static final double SLOWSPEED = 0.5;
+	
+	Talon leftController;
+	Talon rightController;
+	
+	public DriveSubsystem()
+	{
+		
+		leftController = new Talon(Constants.LEFT_DRIVE_CONTROLLER_PORT);
+		leftController = new Talon(Constants.RIGHT_DRIVE_CONTROLLER_PORT);
+	}
+	
+	public void periodic(float leftMagnitude, float rightMagnitude, boolean checkSlowSpeed, float checkControllerInputs)
+	{
+		if((leftMagnitude >= DEADZONE || leftMagnitude <= (-1 * DEADZONE)) || )
+		
+	}
+	
+	public void checkSlowmode()
+	{
+		
+	}
+	
+	public void checkGearshit()
+	{
+		
+	}
+	
+	
+	
+}
