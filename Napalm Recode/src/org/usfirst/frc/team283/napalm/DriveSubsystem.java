d5f9f79ccf1b08cbf439ddc184e3673f390584ad
TJ <bakugantj@gmail.com>
2017-07-10 14:44:17 -0400

    Finished drivetrain and lift
    Also added required constants
    	-TJ

---
+++ Napalm Recode/src/org/usfirst/frc/team283/napalm/DriveSubsystem.java
@@ -0,0 +1,61 @@
+package org.usfirst.frc.team283.napalm;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Spark;
+
+public class DriveSubsystem
+{
+	//Constants
+	private static final double DEADZONE = 0.1;
+	private static final double SLOWSPEED = 0.5;
+	//Variable States
+	private static boolean storedState = false;	
+	//Motors
+	Spark leftController;
+	Spark rightController;
+	//Solenoids
+	Solenoid gearShift;
+	//Subsystems
+	Lift Lift;
+	public DriveSubsystem()
+	{
+		leftController = new Spark(Constants.LEFT_DRIVE_CONTROLLER_PORT);
+		rightController = new Spark(Constants.RIGHT_DRIVE_CONTROLER_PORT);
+		gearShift = new Solenoid(Constants.SPEED_SHIFT_SOLENOID_PORT);
+		Lift = new Lift();
+	}
+	
+	public void periodic(float leftMagnitude, float rightMagnitude, float triggerMagnitude, boolean slowSpeed, boolean buttonState)
+	{
+		Lift.periodic(triggerMagnitude);
+		if(buttonState == true && storedState == false)
+		{
+			gearShift.set(!gearShift.get());
+		}
+		storedState = buttonState;
+		if(((leftMagnitude >= DEADZONE) || (leftMagnitude <= (-1 * DEADZONE))) && slowSpeed == false)
+		{
+			leftController.set(leftMagnitude);
+		}
+		else if(((leftMagnitude >= DEADZONE) || (leftMagnitude <= (-1 * DEADZONE))) && slowSpeed == true)
+		{
+			leftController.set(SLOWSPEED * leftMagnitude);
+		}
+		else
+		{
+			leftController.set(0);
+		}
+		if(((rightMagnitude >= DEADZONE)||(rightMagnitude <= (-1 * DEADZONE))) && slowSpeed == false)
+		{
+			rightController.set(rightMagnitude);
+		}
+		else if(((rightMagnitude >= DEADZONE)||(rightMagnitude <= (-1 * DEADZONE))) && slowSpeed == true)
+		{
+			rightController.set(SLOWSPEED * rightMagnitude);
+		}
+		else
+		{
+			rightController.set(0);
+		}
+	}
+}
---
+++ Napalm Recode/src/org/usfirst/frc/team283/napalm/Napalm.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team283.napalm;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class Napalm extends IterativeRobot //Post-Release merge comment
+{
+	//Intermediary Comment
+	DriveSubsystem drivetrain;
+	
+	@Override
+	public void robotInit() 
+	{
+		drivetrain = new DriveSubsystem();
+	}
+
+	@Override
+	public void autonomousInit() 
+	{
+		
+	}
+
+	@Override
+	public void autonomousPeriodic() 
+	{
+		
+	}
+
+	@Override
+	public void teleopInit() 
+	{
+		
+	}
+	
+	@Override
+	public void teleopPeriodic() 
+	{
+		
+	}
+}
+
