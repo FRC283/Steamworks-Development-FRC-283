5d4bee57755523f285d948f33d80462bdfea88d7
TJ <bakugantj@gmail.com>
2017-07-10 18:05:05 -0400

    Tesing Home
    Changes made at school to drive and gear
    	-TJ

--- Napalm Recode/src/org/usfirst/frc/team283/napalm/GearSubsystem.java
+++ Napalm Recode/src/org/usfirst/frc/team283/napalm/GearSubsystem.java
@@ -1,5 +1,50 @@
 package org.usfirst.frc.team283.napalm;
 
-public class GearSubsystem {
+import edu.wpi.first.wpilibj.Solenoid;
 
+public class GearSubsystem 
+{
+	//Solenoids
+	Solenoid gate;
+	Solenoid push;
+	Solenoid pouch;
+	//Timers
+	
+	
+	public GearSubsystem()
+	{
+		gate = new Solenoid(Constants.GEAR_GATE_SOLENOID_PORT);
+		push = new Solenoid(Constants.GEAR_PUSH_SOLENOID_PORT);
+		pouch = new Solenoid(Constants.GEAR_POUCH_SOLENOID_PORT);
+	}
+	public void periodic(boolean gateSol, boolean pushSol, boolean pouchSol)
+	{
+		if(gateSol == true)
+		{
+			gate.set(true);
+		}
+		else
+		{
+			gate.set(false);
+		}
+		if(pushSol == true)
+		{
+			push.set(true);
+		}
+		else
+		{
+			push.set(false);
+		}
+		if(pouchSol == true)
+		{
+			pouch.set(true);
+		}
+		else
+		{
+			pouch.set(false);
+		}
+	}
+	
+	
+	
 }
