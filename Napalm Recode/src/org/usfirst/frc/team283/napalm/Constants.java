bee2c5adf87d39e2c6a293625a18a59477510228
TJ <bakugantj@gmail.com>
2017-07-27 14:30:23 -0400

    Junk Commit
    -TJ

--- Napalm Recode/src/org/usfirst/frc/team283/napalm/Constants.java
+++ Napalm Recode/src/org/usfirst/frc/team283/napalm/Constants.java
@@ -3,29 +3,9 @@ package org.usfirst.frc.team283.napalm;
 public abstract class Constants 
 {
 	//Computer Ports
-		//Controller Ports
-			public static final int DRIVER_CONTROLLER_PORT = 0;
-			public static final int OPERATOR_CONTROLLER_PORT = 1;
+	
 	//Robot Ports
 		//PWM
-			public static final int LEFT_DRIVE_CONTROLLER_PORT = 0;
+			public static final int LEFT_DRIVE_CONTROLER_PORT = 0;
 			public static final int RIGHT_DRIVE_CONTROLER_PORT = 1;
-			public static final int TURRET_CONTROLLER_PORT = 3;
-			public static final int HOPPER_CONTROLLER_PORT = 4;
-			public static final int CLIMB_CONTROLLER_PORT = 5;
-			public static final int INTAKE_CONTROLLER_PORT = 6;
-			public static final int INFEED_CONTROLLER_PORT = 7;
-		//PCM
-			public static final int SPEED_SHIFT_SOLENOID_PORT = 0;
-			public static final int GEAR_PUSH_SOLENOID_PORT = 1;
-			public static final int GEAR_POUCH_SOLENOID_PORT = 2;
-			public static final int GEAR_GATE_SOLENOID_PORT = 3;
-			public static final int HOOD_DEFLECTOR_SOLENOID_PORT = 4;
-		//DIO
-			public static final int LEFT_DRIVE_ENCODER_PORT_A = 0;
-			public static final int LEFT_DRIVE_ENCODER_PORT_B = 1;
-			public static final int CW_LIMIT_SWITCH_PORT = 2;
-			public static final int CCW_LIMIT_SWITCH_PORT = 3;
-			public static final int RIGHT_DRIVE_ENCODER_PORT_A = 7;
-			public static final int RIGHT_DRIVE_ENCODER_PORT_B = 8;
 }
--- Napalm Recode/src/org/usfirst/frc/team283/napalm/DriveSubsystem.java
+++ Napalm Recode/src/org/usfirst/frc/team283/napalm/DriveSubsystem.java
@@ -1,68 +1,39 @@
 package org.usfirst.frc.team283.napalm;
 
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Talon;
 
 public class DriveSubsystem
-{				//Test
+{
 	//Constants
 	private static final double DEADZONE = 0.1;
 	private static final double SLOWSPEED = 0.5;
-	//Variable States
-	private static boolean slowspeedBuffer = false;	
-	//Motors
-	Spark leftController;
-	Spark rightController;
-	//Solenoids
-	Solenoid gearShift;
-	//Subsystems
-	Lift Lift;
+	
+	Talon leftController;
+	Talon rightController;
+	
 	public DriveSubsystem()
 	{
-		leftController = new Spark(Constants.LEFT_DRIVE_CONTROLLER_PORT);
-		rightController = new Spark(Constants.RIGHT_DRIVE_CONTROLER_PORT);
-		gearShift = new Solenoid(Constants.SPEED_SHIFT_SOLENOID_PORT);
-		Lift = new Lift();
+		
+		leftController = new Talon(Constants.LEFT_DRIVE_CONTROLLER_PORT);
+		leftController = new Talon(Constants.RIGHT_DRIVE_CONTROLLER_PORT);
 	}
 	
-	public void periodic(float leftMagnitude, float rightMagnitude, float triggerMagnitude, boolean slowSpeed, boolean buttonState)
+	public void periodic(float leftMagnitude, float rightMagnitude, boolean checkSlowSpeed, float checkControllerInputs)
 	{
-		Lift.periodic(triggerMagnitude);
-		if(buttonState == true && slowspeedBuffer == false)
-		{
-			gearShift.set(!gearShift.get());
-		}
-		slowspeedBuffer = buttonState;
+		if((leftMagnitude >= DEADZONE || leftMagnitude <= (-1 * DEADZONE)) || )
 		
-		if(slowSpeed == false)
-		{
-			leftController.set(Rescaler.rescale(DEADZONE, 1.0, 0.0, 1.0, leftMagnitude));
-			rightController.set(Rescaler.rescale(DEADZONE, 1.0, 0.0, 1.0, rightMagnitude));
-		}
-		
-		/*if(((leftMagnitude >= DEADZONE) || (leftMagnitude <= (-1 * DEADZONE))) && slowSpeed == false)
-		{
-			leftController.set(leftMagnitude);
-		}
-		else if(((leftMagnitude >= DEADZONE) || (leftMagnitude <= (-1 * DEADZONE))) && slowSpeed == true)
-		{
-			leftController.set(SLOWSPEED * leftMagnitude);
-		}
-		else
-		{
-			leftController.set(0);
-		}
-		if(((rightMagnitude >= DEADZONE)||(rightMagnitude <= (-1 * DEADZONE))) && slowSpeed == false)
-		{
-			rightController.set(rightMagnitude);
-		}
-		else if(((rightMagnitude >= DEADZONE)||(rightMagnitude <= (-1 * DEADZONE))) && slowSpeed == true)
-		{
-			rightController.set(SLOWSPEED * rightMagnitude);
-		}
-		else
-		{
-			rightController.set(0);
-		}*/
 	}
+	
+	public void checkSlowmode()
+	{
+		
+	}
+	
+	public void checkGearshit()
+	{
+		
+	}
+	
+	
+	
 }
