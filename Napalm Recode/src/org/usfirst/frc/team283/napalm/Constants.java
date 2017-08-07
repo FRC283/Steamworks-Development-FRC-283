6f2f67e50f05791b4205066151c5ec7e0801fcad
Ben Ranson <benr444@bellsouth.net>
2017-08-07 15:27:42 -0400

    Trying to resolve conflict in code

--- Napalm Recode/src/org/usfirst/frc/team283/napalm/Constants.java
+++ Napalm Recode/src/org/usfirst/frc/team283/napalm/Constants.java
@@ -3,21 +3,20 @@ package org.usfirst.frc.team283.napalm;
 public abstract class Constants 
 {
 	//Computer Ports
-<<<<<<< HEAD
 		public static final int DRIVER_CONTROLLER_PORT = 0;
 		public static final int OPERATOR_CONTROLLER_PORT = 1;
 	//Controller Ports
 		//Buttons
-			public static final int A = 0;
-			public static final int B = 1;
-			public static final int X = 2;
-			public static final int Y = 3;
-			public static final int LEFT_BUMPER = 4;
-			public static final int RIGHT_BUMPER = 5;
-			public static final int BACK = 6;
-			public static final int START = 7;
-			public static final int LEFT_STICK_BUTTON = 8;
-			public static final int RIGHT_STICK_BUTTON = 9;
+			public static final int A = 1;
+			public static final int B = 2;
+			public static final int X = 3;
+			public static final int Y = 4;
+			public static final int LEFT_BUMPER = 5;
+			public static final int RIGHT_BUMPER = 6;
+			public static final int BACK = 7;
+			public static final int START = 8;
+			public static final int LEFT_STICK_BUTTON = 9;
+			public static final int RIGHT_STICK_BUTTON = 10;
 		//Sticks
 			public static final int LEFT_X = 0;
 			public static final int LEFT_Y = 1;
@@ -25,14 +24,10 @@ public abstract class Constants
 			public static final int RIGHT_TRIGGER = 3;
 			public static final int RIGHT_X = 4;
 			public static final int RIGHT_Y = 5;
-=======
-	
->>>>>>> branch 'master' of https://github.com/Benr444/Napalm-Java-FRC-283
 	//Robot Ports
 		//PWM
-			public static final int LEFT_DRIVE_CONTROLER_PORT = 0;
+			public static final int LEFT_DRIVE_CONTROLLER_PORT = 0;
 			public static final int RIGHT_DRIVE_CONTROLER_PORT = 1;
-<<<<<<< HEAD
 			public static final int TURRET_CONTROLLER_PORT = 3;
 			public static final int HOPPER_CONTROLLER_PORT = 4;
 			public static final int CLIMB_CONTROLLER_PORT = 5;
@@ -51,6 +46,4 @@ public abstract class Constants
 			public static final int CCW_LIMIT_SWITCH_PORT = 3;
 			public static final int RIGHT_DRIVE_ENCODER_PORT_A = 7;
 			public static final int RIGHT_DRIVE_ENCODER_PORT_B = 8;
-=======
->>>>>>> branch 'master' of https://github.com/Benr444/Napalm-Java-FRC-283
 }
--- Napalm Recode/src/org/usfirst/frc/team283/napalm/DriveSubsystem.java
+++ Napalm Recode/src/org/usfirst/frc/team283/napalm/DriveSubsystem.java
@@ -1,28 +1,20 @@
+
 package org.usfirst.frc.team283.napalm;
-<<<<<<< HEAD
+import org.usfirst.frc.team283.napalm.Scheme.Schema;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Spark;
-=======
-
-import edu.wpi.first.wpilibj.Talon;
->>>>>>> branch 'master' of https://github.com/Benr444/Napalm-Java-FRC-283
 
 public class DriveSubsystem
-<<<<<<< HEAD
 {				
-=======
-{
->>>>>>> branch 'master' of https://github.com/Benr444/Napalm-Java-FRC-283
 	//Constants
 	/** Minimum value for driving on logitech axis */
 	private static final double DEADZONE = 0.1;
 	/** Minimum value for logitech trigger for climb */
-	private static final double TRIGGER_DEADZONE = 0.1;
+	private static final double TRIGGER_DEADZONE = 0.5;
 	/** The value motor speeds are multiplied by when slowspeed is enabled */
 	private static final double SLOWSPEED = 0.5;
 	
-<<<<<<< HEAD
 	/** Stores the previous (last-cycle) value of the gear shift button's state */
 	private boolean gearShiftBuffer = false;	
 
@@ -30,28 +22,20 @@ public class DriveSubsystem
 	Spark rightController;
 	Spark climbSpark;
 	Solenoid gearShift;
-=======
-	Talon leftController;
-	Talon rightController;
->>>>>>> branch 'master' of https://github.com/Benr444/Napalm-Java-FRC-283
 	
 	public DriveSubsystem()
 	{
-<<<<<<< HEAD
 		leftController = new Spark(Constants.LEFT_DRIVE_CONTROLLER_PORT);
 		rightController = new Spark(Constants.RIGHT_DRIVE_CONTROLER_PORT);
 		climbSpark = new Spark(Constants.CLIMB_CONTROLLER_PORT);
 		
 		gearShift = new Solenoid(Constants.SPEED_SHIFT_SOLENOID_PORT);
 		
-=======
-		
-		leftController = new Talon(Constants.LEFT_DRIVE_CONTROLLER_PORT);
-		leftController = new Talon(Constants.RIGHT_DRIVE_CONTROLLER_PORT);
->>>>>>> branch 'master' of https://github.com/Benr444/Napalm-Java-FRC-283
 	}
 	
-<<<<<<< HEAD
+	@Schema(Scheme.LEFT_Y)
+	@Schema(Scheme.RIGHT_Y)
+	@Schema(value = Scheme.RIGHT_TRIGGER, desc = SLOWSPEED + " speed")
 	/**
 	 * 
 	 * @param leftMagnitude
@@ -59,15 +43,12 @@ public class DriveSubsystem
 	 * @param slowSpeed
 	 */
 	public void drive(double leftMagnitude, double rightMagnitude, boolean slowSpeed)
-=======
-	public void periodic(float leftMagnitude, float rightMagnitude, boolean checkSlowSpeed, float checkControllerInputs)
->>>>>>> branch 'master' of https://github.com/Benr444/Napalm-Java-FRC-283
 	{
-<<<<<<< HEAD
-		leftController.set( (Rescaler.rescale(DEADZONE, 1.0, 0.0, 1.0, leftMagnitude)) * (slowSpeed ? SLOWSPEED : 1));
+		leftController.set(-1 * (Rescaler.rescale(DEADZONE, 1.0, 0.0, 1.0, leftMagnitude)) * (slowSpeed ? SLOWSPEED : 1));
 		rightController.set( (Rescaler.rescale(DEADZONE, 1.0, 0.0, 1.0, rightMagnitude)) * (slowSpeed ? SLOWSPEED : 1));
 	}
 	
+	@Schema(Scheme.LEFT_BUMPER)
 	/**
 	 * 
 	 * @param gearButtonState
@@ -79,36 +60,15 @@ public class DriveSubsystem
 			gearShift.set(!gearShift.get());
 		}
 		gearShiftBuffer = gearButtonState;
-=======
-		if((leftMagnitude >= DEADZONE || leftMagnitude <= (-1 * DEADZONE)) || )
-		
->>>>>>> branch 'master' of https://github.com/Benr444/Napalm-Java-FRC-283
 	}
 	
-<<<<<<< HEAD
+	//ON XBOX @Schema(JoystickSchema.RIGHT_TRIGGER)
 	/**
 	 * 
 	 * @param triggerMagnitude
 	 */
 	public void lift(double triggerMagnitude)
 	{
-		if(triggerMagnitude > TRIGGER_DEADZONE) //If the trigger is pushed down past a certain point
-		{
-			climbSpark.set(-1 * triggerMagnitude); //Set to climb the value of the trigger (Reverse-wired motor)
-		}
+		climbSpark.set(triggerMagnitude > TRIGGER_DEADZONE ? -1 * triggerMagnitude : 0); //Set to climb the value of the trigger (Reverse-wired motor)
 	}
-=======
-	public void checkSlowmode()
-	{
-		
-	}
-	
-	public void checkGearshit()
-	{
-		
-	}
-	
-	
-	
->>>>>>> branch 'master' of https://github.com/Benr444/Napalm-Java-FRC-283
 }
