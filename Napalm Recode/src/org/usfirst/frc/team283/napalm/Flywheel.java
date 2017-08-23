25a7ac6f08c34f5e2a685d8f72d7f5be524cf920
FRC-283-PROG-2 <frcprogramming283.2@gmail.com>
2017-08-23 10:11:43 -0400

    End - of - Day
    Ben
    Flywheel coding

--- Napalm Recode/src/org/usfirst/frc/team283/napalm/Flywheel.java
+++ Napalm Recode/src/org/usfirst/frc/team283/napalm/Flywheel.java
@@ -38,54 +38,105 @@ public class Flywheel
 	private double powerAccel;
 	
 	/** The speed value we are attempting to approach. */
-	private double targetSpeed;
+	private double targetRPM;
 	
 	/** Value added per robot cycle to flywheel speed target */
-	private double speedAccel;
+	private double rpmAccel;
 	
-	/** Whether or not we are currently trying to approach a target. */
+	/** Whether or not we are currently trying to approach a target rpm. */
 	private boolean isTargeting = false;
 	
-	
-	public Flywheel()
+	/**
+	 * 
+	 * @param c - Motor controller that controls flywheel - can be any class that extends this type
+	 * @param e - Encoder that is attached to flywheel
+	 */
+	public Flywheel(PWMSpeedController c, Encoder e)
 	{
-		
+		this.controller = c;
+		this.tach = e;
 	}
 
 	/** Called once per robot cycle */
 	public void periodic()
 	{
-		
+		if (this.isTargeting == true) //Performs p-control to approach given rpm
+		{
+			this.targetRPM += this.rpmAccel; //Add on speed accel
+			//Put p-control here
+		}
+		else
+		{
+			this.power += this.powerAccel; //If we are not targeting a speed, add power accel
+		}
+		this.controller.set(this.power);
 	}
 	
 	/**
-	 * Sets the motor power to the given number
+	 * Sets the motor power to the given number <br>
+	 * Calling this function stops any speed targeting and stops power acceleration
 	 * @param power - number to set power to
 	 */
 	public void setPower(double power)
 	{
 		this.power = power;
+		this.powerAccel = 0;
+		this.isTargeting = false;
 	}
 	
 	/**
-	 * The power to the flywheel changes by the given amount per cycle
+	 * The power to the flywheel changes by the given amount per cycle <br>
+	 * Calling this function stops any speed targeting
 	 * @param delta - change in power per cycle
 	 */
 	public void setPowerAccel(double accel)
 	{
 		this.powerAccel = accel;
+		this.isTargeting = false;
 	}
 	
-	public void setTargetSpeed(double rpm)
+	/**
+	 * Causes the robot to use p-control to achieve the given RPM <br>
+	 * Kills any current speedAccel
+	 * @param rpm - the RPM to approach ("target")
+	 */
+	public void setTargetRPM(double rpm)
 	{
 		this.targetSpeed = rpm;
+		this.speedAccel = 0;
 		this.isTargeting = true;
 	}
 	
-	public void setTargetSpeedAccel(double rpmChange)
+	/**
+	 * Each robot cycle, the robot will add the given amount onto the target RPM
+	 * @param rpmChange - RPM added per cycle
+	 */
+	public void setTargetRPMAccel(double rpmChange)
 	{
 		this.speedAccel = rpmChange;
+		this.isTargeting = true;
 	}
+	
+	public void brake()
+	{
+		
+	}
+	
+	public void setBrake()
+	{
+		
+	}
+	
+	public double getTargetRPM()
+	{
+		this.targetSpeed;
+	}
+	
+	public double getRPM()
+	{
+		
+	}
+	
 	
 	/**
 	 * Calling once reverses all values set and received from the controller <br>
@@ -93,7 +144,7 @@ public class Flywheel
 	 */
 	public void reverseController()
 	{
-		this.controllerPolarity = -1; //Do not change to "!value"
+		this.controllerPolarity = -1; //Do not change to "!value" or similar
 	}
 	
 	/**
@@ -102,7 +153,7 @@ public class Flywheel
 	 */
 	public void reverseTach()
 	{
-		this.tachPolarity = -1; //Do not change to "!value"
+		this.tachPolarity = -1; //Do not change to "!value" or similar
 	}
 	
 }
