1ff587314747152648bf5397a70bbe92bde89cd5
FRC-283-PROG-2 <frcprogramming283.2@gmail.com>
2017-08-28 14:05:19 -0400

    Flywheel Updates
    Ben
    Need to add power-based control

--- Napalm Recode/src/org/usfirst/frc/team283/napalm/ShooterSubsystem.java
+++ Napalm Recode/src/org/usfirst/frc/team283/napalm/ShooterSubsystem.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team283.napalm;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Spark;
+import org.usfirst.frc.team283.napalm.Constants;
 
 public class ShooterSubsystem 
 {
@@ -19,7 +21,15 @@ public class ShooterSubsystem
 	{
 		Hopper hopper = new Hopper();
 		Flywheel flywheel = new Flywheel(new Spark(999), new Encoder(999, 999), 360);
-		TurretAxis turret = new TurretAxis(new Spark(998), new Encoder(999, 999), 200, 400).addLimits(min, max);.configureTargeting(k, e);
+		TurretAxis turret = new TurretAxis(new Spark(998), new Encoder(999, 999), 200, 400);
+		turret.addLimits(new DigitalInput(CCW_LIMIT_SWITCH_PORT), new DigitalInput(CW_LIMIT_SWITCH_PORT));
+		turret.configureTargeting(1/1000, 50);
+	}
+	
+	public void periodic()
+	{
+		flywheel.periodic();
+		turret.periodic();
 	}
 	
 	public void feed(boolean buttonState) //Runs the hopper and feed motors towards the flywheel at a fixed rate
